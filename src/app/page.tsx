7e2da4940f292e61ef41031b0004177699c447f4
Jeremiah Louis <obobairibhojie19.jeremiah@edouniversity.edu.ng>
2025-05-27 00:24:33 +0100

    Updated ESLint configuration to disable specific rules and modified the Home component by uncommenting the main structure and removing unused code snippets for improved clarity.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,10 @@ import { FileTabs } from "@/components/playground-section/FileTabs"
 
 export default function Home() {
   return (
-    // <main className="relative min-h-screen flex flex-col">
     <main className="relative min-h-screen flex flex-col">
       <Header />
       <div className="flex-1 flex flex-col">
         <Hero />
-        {/* <UrlToMarkdown /> */}
         <div className="bg-[#F7F7F7] dark:bg-black/10 rounded-3xl p-6 sm:p-8 sm:mx-16 my-10">
           <h1 className="text-4xl font-semibold tracking-tighter text-center mb-3">Start <span className="text-blue-500 dark:text-blue-400 italic">Scraping</span> your data now</h1>
           <p className="text-center tracking-tighter text-gray-600 mb-8">Scraping your data with AI shouldn't be hard. We make it easy.</p>
